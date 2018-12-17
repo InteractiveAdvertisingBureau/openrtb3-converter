6326666ff673a0f0197d754227f53d2b76cf0b25
shiva.bhalla <shiva.b@media.net>
2018-12-17 10:35:22 +0530

    mappings for response

--- src/main/java/net/media/mapper/OpenRtb24To3Mapper.java
+++ src/main/java/net/media/mapper/OpenRtb24To3Mapper.java
@@ -2,8 +2,14 @@ package net.media.mapper;
 
 import net.media.openrtb24.request.BidRequest;
 import net.media.openrtb24.request.Imp;
+import net.media.openrtb24.response.BidResponse;
+import net.media.openrtb24.response.SeatBid;
+import net.media.openrtb3.Ad;
+import net.media.openrtb3.Bid;
 import net.media.openrtb3.Item;
 import net.media.openrtb3.Request;
+import net.media.openrtb3.Response;
+import net.media.openrtb3.Seatbid;
 import net.media.openrtb3.Source;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -19,4 +25,28 @@ public interface OpenRtb24To3Mapper {
     @Mapping(source = "imp.bidfloorcur", target = "flrcur"),
   })
   Item map(Imp imp);
+
+  @Mappings({
+  })
+  Response map(BidResponse bidResponse);
+
+  @Mappings({
+    @Mapping(source = "seat", target = "bidResponse.seat"),
+    @Mapping(source = "package", target = "bidResponse.group"),
+  })
+  Seatbid seatMapper(BidResponse bidResponse);
+
+  @Mappings({
+    @Mapping(source = "purl", target = "nurl"),
+  })
+  Bid map(net.media.openrtb24.response.Bid bid);
+
+  @Mappings({
+    @Mapping(source = "id", target = "crid"),
+    @Mapping(source = "lang", target = "language"),
+    @Mapping(source = "lang", target = "language"),
+  })
+  Ad adMapper(net.media.openrtb24.response.Bid bid);
+
+
 }
---
+++ src/main/java/net/media/openrtb3/Ad.java
@@ -0,0 +1,7 @@
+package net.media.openrtb3;
+
+/**
+ * Created by shiva.b on 17/12/18.
+ */
+public class Ad {
+}
