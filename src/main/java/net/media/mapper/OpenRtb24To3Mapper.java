597799ef529afbe75814281907ac8e84db03ea84
shiva.bhalla <shiva.b@media.net>
2018-12-17 13:38:23 +0530

    ad object

--- src/main/java/net/media/mapper/OpenRtb24To3Mapper.java
+++ src/main/java/net/media/mapper/OpenRtb24To3Mapper.java
@@ -5,7 +5,9 @@ import net.media.openrtb24.request.Imp;
 import net.media.openrtb24.response.BidResponse;
 import net.media.openrtb24.response.SeatBid;
 import net.media.openrtb3.Ad;
+import net.media.openrtb3.Banner;
 import net.media.openrtb3.Bid;
+import net.media.openrtb3.Display;
 import net.media.openrtb3.Item;
 import net.media.openrtb3.Request;
 import net.media.openrtb3.Response;
@@ -27,6 +29,7 @@ public interface OpenRtb24To3Mapper {
   Item map(Imp imp);
 
   @Mappings({
+    @Mapping(source="cdata", target = "ext.cdata"),
   })
   Response map(BidResponse bidResponse);
 
@@ -37,16 +40,33 @@ public interface OpenRtb24To3Mapper {
   Seatbid seatMapper(BidResponse bidResponse);
 
   @Mappings({
-    @Mapping(source = "purl", target = "nurl"),
+    @Mapping(target = "purl", source = "nurl"),
+    @Mapping(target = "deal", source = "dealid"),
+    @Mapping(target = "tactic", source = "ext.tactic"),
+    @Mapping(target = "burl", source = "ext.burl"),
+    @Mapping(target = "lurl", source = "lurl"),
+    @Mapping(target = "mid", source = "ext.mid"),
+    @Mapping(target = "media", source = "ext.media")
   })
   Bid map(net.media.openrtb24.response.Bid bid);
 
   @Mappings({
-    @Mapping(source = "id", target = "crid"),
-    @Mapping(source = "lang", target = "language"),
-    @Mapping(source = "lang", target = "language"),
+    @Mapping(target = "id", source = "crid"),
+    @Mapping(target = "lang", source = "language"),
+    @Mapping(target = "cattax", source = "ext.cattax"),
+    @Mapping(target = "secure", source = "ext.secure"),
+    @Mapping(target = "mrating", source = "ext.mrating"),
+    @Mapping(target = "init", source = "ext.init"),
+    @Mapping(target = "lastmod", source = "ext.lastmod")
   })
   Ad adMapper(net.media.openrtb24.response.Bid bid);
 
 
+  @Mappings({
+    @Mapping(source = "ext.ctype", target="ctype")
+  })
+  Display displayMapper(net.media.openrtb24.response.Bid bid);
+
+
+  Banner bannerMapper(net.media.openrtb24.response.Bid bid);
 }
