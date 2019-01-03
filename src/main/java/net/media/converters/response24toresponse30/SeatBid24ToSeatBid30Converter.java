5f97621a2231e47b5e0674ed48537c924d09e20a
shiva.bhalla <shiva.b@media.net>
2019-01-03 18:27:01 +0530

    24To30 aadha rasta

---
+++ src/main/java/net/media/converters/response24toresponse30/SeatBid24ToSeatBid30Converter.java
@@ -0,0 +1,65 @@
+package net.media.converters.response24toresponse30;
+
+import net.media.config.Config;
+import net.media.converters.Converter;
+import net.media.openrtb24.response.Bid;
+import net.media.openrtb24.response.SeatBid;
+import net.media.openrtb3.Seatbid;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Objects.nonNull;
+import static org.apache.commons.collections.CollectionUtils.isEmpty;
+
+/**
+ * @author shiva.b
+ */
+public class SeatBid24ToSeatBid30Converter implements Converter<SeatBid, Seatbid> {
+
+  private Converter<Bid, net.media.openrtb3.Bid> bid24ToBid30Converter;
+
+  public SeatBid24ToSeatBid30Converter(Converter<Bid, net.media.openrtb3.Bid> bid24ToBid30Converter) {
+    this.bid24ToBid30Converter = bid24ToBid30Converter;
+  }
+
+  @Override
+  public Seatbid map(SeatBid source, Config config) {
+    if ( source == null ) {
+      return null;
+    }
+    Seatbid seatbid = new Seatbid();
+    inhance(source, seatbid, config);
+    return seatbid;
+  }
+
+  @Override
+  public void inhance(SeatBid source, Seatbid target, Config config) {
+    if (source == null || target == null) {
+      return;
+    }
+    Seatbid seatbid = new Seatbid();
+
+    Map<String, Object> map = source.getExt();
+    if ( map != null ) {
+      seatbid.setExt(new HashMap<>(map) );
+    }
+    else {
+      seatbid.setExt(null);
+    }
+    seatbid.set_package(source.getGroup());
+    seatbid.setSeat(source.getSeat());
+    if (!isEmpty(source.getBid())) {
+      List<net.media.openrtb3.Bid> bidList = new ArrayList<>();
+      for (Bid bid : source.getBid()) {
+        net.media.openrtb3.Bid bid30 = bid24ToBid30Converter.map(bid, config);
+        if (nonNull(bid)) {
+          bidList.add(bid30);
+        }
+      }
+      seatbid.setBid(bidList);
+    }
+  }
+}
