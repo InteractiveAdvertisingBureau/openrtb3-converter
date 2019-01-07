8db71a07b9276ddffd9fd76ef3fc21319babf98f
Ankit Choudhary <ankit.cho@media.net>
2019-01-07 17:07:44 +0530

    errors fixed

--- converter-core/src/main/java/net/media/OpenRtbConverter.java
+++ converter-core/src/main/java/net/media/OpenRtbConverter.java
@@ -86,7 +86,7 @@ public class OpenRtbConverter {
   }
 
   private boolean shouldValidate(Config overridingConfig) {
-    return  overridingConfig.isValidate();
+    return  overridingConfig.getValidate();
   }
 
 }
