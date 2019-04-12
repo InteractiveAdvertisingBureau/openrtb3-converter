40f80ff428bb6e806d38fb5bb66b003000d43b71
abhijeet.an <abhijeet.an@media.net>
2019-04-12 18:01:08 +0530

    request side master json tests done

--- converter-core/src/main/java/net/media/converters/request30toRequest25/ItemToImpConverter.java
+++ converter-core/src/main/java/net/media/converters/request30toRequest25/ItemToImpConverter.java
@@ -339,14 +339,20 @@ public class ItemToImpConverter implements Converter<Item, Imp> {
         if (isNull(imp.getExt())) {
           imp.setExt(new HashMap<>());
         }
-        imp.getExt().put("ssai", item.getSpec().getPlacement().getSsai());
-        imp.getExt().put("reward", item.getSpec().getPlacement().getReward());
-        imp.getExt().put("admx", item.getSpec().getPlacement().getAdmx());
-        imp.getExt().put("curlx", item.getSpec().getPlacement().getCurlx());
+        if(item.getSpec().getPlacement().getSsai() != null)
+          imp.getExt().put("ssai", item.getSpec().getPlacement().getSsai());
+        if(item.getSpec().getPlacement().getReward() != null)
+          imp.getExt().put("reward", item.getSpec().getPlacement().getReward());
+        if(item.getSpec().getPlacement().getAdmx() != null)
+          imp.getExt().put("admx", item.getSpec().getPlacement().getAdmx());
+        if(item.getSpec().getPlacement().getCurlx() != null)
+          imp.getExt().put("curlx", item.getSpec().getPlacement().getCurlx());
       }
       if (nonNull(item.getSpec().getPlacement().getDisplay())) {
-        imp.getExt().put("ampren", item.getSpec().getPlacement().getDisplay().getAmpren());
-        imp.getExt().put("event", item.getSpec().getPlacement().getDisplay().getEvent());
+        if(item.getSpec().getPlacement().getDisplay().getAmpren() != null)
+          imp.getExt().put("ampren", item.getSpec().getPlacement().getDisplay().getAmpren());
+        if(item.getSpec().getPlacement().getDisplay().getEvent() != null)
+          imp.getExt().put("event", item.getSpec().getPlacement().getDisplay().getEvent());
       }
     }
   }
